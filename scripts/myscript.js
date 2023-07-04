7e397d43606fb7f19172f9e0121e2928176b9005
Dimitar Cvetanoski <dimitar.cv23@gmail.com>
2023-07-05 01:16:29 +0200

    Finished the game logic

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -8,42 +8,50 @@ function getComputerChoice(){
 let playerParagraph = document.querySelector('#player');
 let computerParagraph = document.querySelector('#computer');
 
+let outcomeParagraph = document.querySelector('#round-outcome');
+
 let playerWins = 0;
 let computerWins = 0;
 
 function playRound(playerSelection, computerSelection) {
-    console.log('clicked');
+    if(playerWins === 5 || computerWins === 5){
+        return;
+    }
     if(playerSelection === computerSelection){
         // it's a tie
         console.log('tie');
-    }
-
-    if(
+        outcomeParagraph.textContent = 'This round is a tie';
+    } else if(
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')
     ){
         playerWins++;
         console.log('playerwin');
-    }
-
-    if(
+        outcomeParagraph.textContent = 'This round is a player win';
+    } else if(
         (computerSelection === 'rock' && playerSelection === 'scissors') ||
         (computerSelection === 'paper' && playerSelection === 'rock') ||
         (computerSelection === 'scissors' && playerSelection === 'paper')
     ){
         computerWins++;
         console.log('computerwin');
+        outcomeParagraph.textContent = 'This round is a computer win';
     }
 
     playerParagraph.textContent = `Player wins: ${playerWins}`;
     computerParagraph.textContent = `Computer wins: ${computerWins}`;
+
+    if(playerWins === 5){
+        outcomeParagraph.textContent = 'Game Over! Player wins the game';
+    } else if(computerWins === 5){
+        outcomeParagraph.textContent = 'Game Over! Computer wins the game';
+    }
 }
 
 const buttonRock = document.querySelector("#rock");
 const buttonPaper = document.querySelector("#paper");
 const buttonScissors = document.querySelector("#scissors");
-
 
 buttonRock.addEventListener('click', () => playRound('rock', getComputerChoice()));
 buttonPaper.addEventListener('click', () => playRound('paper', getComputerChoice()));
